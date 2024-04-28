7c97e7b6b2f8c62dfc4f0fe4ca4dfdfe04d85b79
haritha117 <harithaa11july@gmail.com>
2024-04-28 15:10:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from 'react';
+import './test.css'
+import Title from './Title';
+
 
 function App() {
+  const [bookTitle, setBookTitle] = useState('');
+  const [recommendations, setRecommendations] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (event) => {
+    setBookTitle(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://192.168.0.104:5000/recommendations', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ book_title: bookTitle }),
+      });
+      const data = await response.json();
+      const recommendationsArray = Object.keys(data).map((key) => data[key]);
+      setRecommendations(recommendationsArray);
+    } catch (error) {
+      console.error('Error:', error);
+      setErrorMessage('Error retrieving recommendations. Please try again.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <form className="form-container" onSubmit={handleSubmit}>
+      <div className="App">
+      <Title />
+    </div>
+        <label>
+          Recommendation For 
+          <div><input type="text" value={bookTitle} onChange={handleInputChange} />
+            </div>
+        </label>
+        <button type="submit">Get Recommendations</button>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </form>
+      <div className="grid-container">
+        {recommendations.map((recommendation, index) => (
+          <div key={index} className="grid-item">
+            <img src={recommendation['Image-URL-S']} alt={recommendation.Title} />
+            <p>Title: {recommendation.Title}</p>
+            <p>Author: {recommendation['Author Name']}</p>
+            <p>Rating: {recommendation['Highest Rating']}</p>
+            <p>Year: {recommendation['Published Year']}</p>
+            <p>Distance: {recommendation.Distance}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      {/* <img src='image.png'/> */}
+      <h1>BOOK RECOMMENDATION SYSTEM</h1>
+    </div>
+  )
+}
+
+export default Title;
